e61e1fb44496f8c14a7d82dff61d21695de10577
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-05-29 16:50:05 +0800

    iframe renderer added

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+	input: ['src/index.js'],
+	output: {
+		file: 'dist/index.js',
+    format: 'es',
+		sourcemap: false
+	},
+	plugins: [
+    resolve(),
+    babel(),
+    terser()
+  ]
+};
