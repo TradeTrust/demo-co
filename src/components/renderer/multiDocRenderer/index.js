9255bbc09901eac8cf538c93fceb90e623e86709
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-06-07 14:25:09 +0800

    active tab ling added

--- src/components/renderer/multiDocRenderer/index.js
+++ src/components/renderer/multiDocRenderer/index.js
@@ -5,14 +5,25 @@ import { connect } from "react-redux";
 import {getTemplates} from "../../../reducers/document";
 
 class MultiDocRenderer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeTab: 0
+        }
+    }
 
-    selectTemplateTab = (idx) => this.props.selectTemplateTab(idx)
+    selectTemplateTab = (idx) => { 
+        this.props.selectTemplateTab(idx);
+        this.setState({ activeTab: idx});
+    }
     render() {
         const { templates} = this.props;
+        const {activeTab} = this.state;
+
         return (
-            <ul className="nav nav-tabs" id="myTab" role="tablist">
+            <ul className="nav nav-tabs container" id="myTab" role="tablist">
                 {templates && templates.length > 0 ? templates.map((t, idx) => <li key={idx} className="nav-item">
-                    <a className="nav-link" id={t.id} onClick={() => this.selectTemplateTab(idx)} role="tab" aria-controls="home" aria-selected="true">{t.label}</a>
+                    <a className={`nav-link ${idx === activeTab ? "active" : ''}`} id={t.id} onClick={() => this.selectTemplateTab(idx)} role="tab" aria-controls="home" aria-selected="true">{t.label}</a>
                 </li>) : null}
             </ul>
         );
