c8efd65b9b6832680f9f5b653d060be931a2f55c
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-06-20 16:50:07 +0800

    chores: refactoring

--- src/components/renderer/reducer/index.js
+++ src/components/renderer/reducer/index.js
@@ -1,17 +1,15 @@
 import { types, initialState } from "./constants";
 
-const { DOCUMENT_TEMPLATE_REGISTER, DOCUMENT_TEMPLATE_SELECT_TAB } = types;
-
 // Reducers
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case DOCUMENT_TEMPLATE_REGISTER:
+    case types.DOCUMENT_TEMPLATE_REGISTER:
       return {
         ...state,
         templates: action.payload,
         activeTemplateTab: 0
       };
-    case DOCUMENT_TEMPLATE_SELECT_TAB:
+    case types.DOCUMENT_TEMPLATE_SELECT_TAB:
       return {
         ...state,
         activeTemplateTab: action.payload
