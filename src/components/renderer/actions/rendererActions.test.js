b3e772adac8143197f5c70e0a54e63ef3b9ae29d
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-06-25 15:06:30 +0800

    lint fix

--- src/components/renderer/actions/rendererActions.test.js
+++ src/components/renderer/actions/rendererActions.test.js
@@ -1,58 +1,54 @@
-import {
-    registerTemplates,
-    getTemplates
-  } from "./";
+import { registerTemplates, getTemplates } from "./";
 
 describe("actions", () => {
-    it("registerTemplates should generate correct action", () => {
-      const fn = registerTemplates;
-      const payload = [
-        {
-          id: "document",
-          label: "Document"
-        },
-        {
-          id: "transcript",
-          label: "Transcript"
-        }
-      ];
-      const expectedAction = {
-        type: "DOCUMENT_TEMPLATE_REGISTER",
-        payload: [
-          { id: "document", label: "Document" },
-          { id: "transcript", label: "Transcript" }
+  it("registerTemplates should generate correct action", () => {
+    const fn = registerTemplates;
+    const payload = [
+      {
+        id: "document",
+        label: "Document"
+      },
+      {
+        id: "transcript",
+        label: "Transcript"
+      }
+    ];
+    const expectedAction = {
+      type: "DOCUMENT_TEMPLATE_REGISTER",
+      payload: [
+        { id: "document", label: "Document" },
+        { id: "transcript", label: "Transcript" }
+      ]
+    };
+    expect(fn(payload)).toEqual(expectedAction);
+  });
+});
+
+describe("selectors", () => {
+  it("getTemplates should return activeTemplateTab", () => {
+    const store = {
+      renderer: {
+        templates: [
+          {
+            id: "document",
+            label: "Document"
+          },
+          {
+            id: "transcript",
+            label: "Transcript"
+          }
         ]
-      };
-      expect(fn(payload)).toEqual(expectedAction);
-    });
+      }
+    };
+    expect(getTemplates(store)).toEqual([
+      {
+        id: "document",
+        label: "Document"
+      },
+      {
+        id: "transcript",
+        label: "Transcript"
+      }
+    ]);
   });
-  
-  describe("selectors", () => {
-  
-    it("getTemplates should return activeTemplateTab", () => {
-      const store = {
-        renderer: {
-          templates: [
-            {
-              id: "document",
-              label: "Document"
-            },
-            {
-              id: "transcript",
-              label: "Transcript"
-            }
-          ]
-        }
-      };
-      expect(getTemplates(store)).toEqual([
-        {
-          id: "document",
-          label: "Document"
-        },
-        {
-          id: "transcript",
-          label: "Transcript"
-        }
-      ]);
-    });
-  });
+});
