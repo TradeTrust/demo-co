2f532ad387b5ef1eb17e856be45516032e8029bf
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-06-26 14:02:32 +0800

    dropzone integration test added

---
+++ src/components/home/dropZone/dropzone-integration.spec.js
@@ -0,0 +1,15 @@
+import { Selector } from "testcafe";
+
+fixture("Dropzone").page`http://localhost:3001`;
+
+const dropzone = Selector("#dropzone");
+
+const validateTextContent = async (t, component, texts) =>
+  texts.reduce(
+    async (_prev, curr) => t.expect(component.textContent).contains(curr),
+    Promise.resolve()
+  );
+
+test("Sample document is rendered correctly", async t => {
+  await validateTextContent(t, dropzone, ["Drag and drop your tradetrust file"]);
+});
